31c41650179a15df2d3784f0812419c56030a04a
chen-qicheng <chen-qicheng@users.noreply.github.com>
2020-06-29 16:03:44 +0800

    add file

--- CommunicationModel/Analysis/analysis_rand2.m
+++ CommunicationModel/Analysis/analysis_rand2.m
@@ -7,11 +7,13 @@ F = 0:Df:fs;            % 功率谱估计的频率分辨率和范围
 
 %数值计算
 xk = sin(2*pi*50*t)+2*sin(2*pi*130*t)+randn(1,length(t));   % 截取时间段上的离散信号样点序列
-Pxx = abs(fft(xk)).^2/(N^2);                                % 功率谱估计
+Pxx = (abs(fft(xk(1:167))).^2/(N^2)+...                     % 功率谱估计
+       abs(fft(xk(168:334))).^2/(N^2)+... 
+       abs(fft(xk(335:501))).^2/(N^2))/3/(N/3)^2;   
 Pav_timedomain = sum(xk.^2)/N;                              % 在时域计算信号功率
 Pav_freqdomain = sum(Pxx);                                  % 通过功率谱计算信号功率
 
 %作功率皮密度图
-plot(F,10*log(Pxx));
+plot(0:3:fs,10*log(Pxx));
 xlabel('频率 Hz');
 ylabel('PSD dB');
